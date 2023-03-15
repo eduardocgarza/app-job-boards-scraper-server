e5b7f0bd19889a0acbc93dbbba830082f92d3755
Eduardo Garza <eduardo@garza.ca>
2023-03-15 13:03:03 -0700

    Initialized app.

---
+++ fl-app-lead-collection/src/pages/InitializeSearchPage/InitializeSearchPage.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function InitializeSearchPage() {
+  return (
+    <div>
+      <h1>InitializeSearchPage</h1>
+    </div>
+  )
+}
---
+++ fl-app-lead-collection/src/pages/MySearchesPage/MySearchesPage.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function MySearchesPage() {
+  return (
+    <div>
+      <h1>MySearchesPage</h1>
+    </div>
+  )
+}
